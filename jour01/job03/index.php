22581352baa435edffafb39e2fd83491035d4e8e
Antoine <antoine.leca@laplateforme.io>
2025-01-13 12:52:39 +0100

    job03 in progress added for testing

---
+++ jour01/job03/index.php
@@ -0,0 +1,32 @@
+<?php 
+    $str = "LaPlateforme";
+    $numd = 1.2;
+    $numh = 1.2e3;
+    $numo = 0186;
+    $numb = 0b1101;
+    $float = 1.234;
+    $bool = true;
+    $bool2 = false;
+    $array = array(1, 2, 3);
+
+    $maxcols = 3;
+    $maxid = 9;
+    $startid = 1;
+
+    echo "<table id="table1">\n";
+    for ($i = $startid; $i <= $maxid; $i++) {
+        echo "<tr>\n";
+        for ($j=1; $j < $maxcols; $j++)
+            if ($startid<=$maxid)
+                echo " <td class="mark">ID".$startid++.</td>\n";
+            else
+                echo " <td> >/td>\n";
+        }
+        echo "</tr>\n<tr>\n";
+        for ($j=1;$j<=$maxcols;$j++)
+            echo "<td>Content</td>\n";
+
+        echo "</tr>\n";
+    }
+echo "</table>\n";
+?>
