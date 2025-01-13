2a7a5a3f9f1bc8a3f585e0d0b0e02024af7cb0e0
Antoine <antoine.leca@laplateforme.io>
2025-01-13 11:17:56 +0100

    adding job01 finalised

--- jour01/job0.9/index.php
+++ jour01/job0.9/index.php
@@ -1,2 +1,7 @@
 <?php 
-echo "Hello la plateforme!";
+
+$hlp = "Hello la plateforme";
+
+echo $hlp;
+
+?>
---
+++ jour01/job01/index.php
@@ -0,0 +1,7 @@
+<?php 
+    $str = "Hello LaPlateforme!";
+    $str2 = "Vive";
+    $str3 = "!";
+
+    echo $str2." LaPlateforme ".$str3;
+?>
