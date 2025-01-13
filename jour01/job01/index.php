29a0396aa373ea5ea209ef5ef0bbac8f8cbf78ed
Antoine <antoine.leca@laplateforme.io>
2025-01-13 11:34:50 +0100

    job correction

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -3,11 +3,12 @@
     $str2 = "Vive";
     $str3 = "!";
 
-    echo $str2." LaPlateforme ".$str3;
+    echo $str2." LaPlateforme ".$str3 ."<br>";
 
     $val = 6;
     echo $val;
     $val += 4;
+    echo $val "<br>";
 
     $myBool = true;
     echo "la variable myBool est " .$myBool;
