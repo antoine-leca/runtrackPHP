67e60f94767b3f1bc8ca47181a358124c1d69de5
Antoine <antoine.leca@laplateforme.io>
2025-01-14 12:57:26 +0100

    adding file job05, testing

---
+++ jour02/job05/index.php
@@ -0,0 +1,11 @@
+<?php
+    $val = 0;
+    while ($val <= 1000):
+        if ($val / 1 or $val / $val) {
+            echo $val;
+        } else {
+            echo "<br>";
+        }
+        $val++;
+    endwhile;
+?>
