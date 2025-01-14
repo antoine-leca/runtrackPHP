56d2500e259c44f077c6640023ae964e968caa4d
Antoine <antoine.leca@laplateforme.io>
2025-01-14 12:27:41 +0100

    adding the step Bold/Underline the number 42, job01 finalised

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -1,7 +1,11 @@
 <?php
     $val = 0;
     while ($val <= 1337):
-        echo $val . "<br>";
+        if ($val == 42) {
+            echo "<b><u>$val</u></b><br>";
+        } else {
+            echo $val . "<br>";
+        }
         $val++;
     endwhile;
 ?>
