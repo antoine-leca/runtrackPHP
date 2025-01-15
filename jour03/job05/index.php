d28143c41cd22379adf9de49b36ea64b087b37a2
Antoine <antoine.leca@laplateforme.io>
2025-01-15 12:14:49 +0100

    adding job05 file, think finalised

--- jour03/job05/index.php
+++ jour03/job05/index.php
@@ -2,25 +2,46 @@
     $str = "On n'est pas le meilleur quand on le croit mais quand on le sait.";
     $voyelles = ["a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y"];
     $str_len = 0;
-    $keyC = 0;
-    $keyV = 0;
+    $voyelles_len = 0;
+    $dic = ["consonnes" => 0, "voyelles" => 0];
     while (isset($str[$str_len])) {
         $str_len++;
     }
+    while (isset($voyelles[$voyelles_len])) {
+        $voyelles_len++;
+    }
     for ($val = 0 ; $val < $str_len ; $val++) {
         $isvoyelle = false;
-        foreach ($voyelles as $voyelle) {
-            if ($str[$val] === $voyelle) {
+        for ($i = 0; $i < $voyelles_len; $i++) {
+            if ($str[$val] === $voyelles[$i]) {
                 $isvoyelle = true;
-                $keyV++;
+                break;
             }
-            else {
-                $isvoyelle = false;
-                $keyC++;
-            }
-            
-            echo $keyV . "<br>";
-            echo $keyC;
+        }
+        if ($isvoyelle) {
+            $dic["voyelles"]++;
+        } else {
+            $dic["consonnes"]++;
         }
     }
 ?>
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <table border="1" cellpadding="10" cellspacing="0">
+            <tr>
+                <th>Consonnes</th>
+                <th>Voyelles</th>
+            </tr>
+            <tr>
+                <td><?php echo $dic["consonnes"]; ?></td>
+                <td><?php echo $dic["voyelles"]; ?></td>
+            </tr>
+        </table>
+    </body>
+    </html>
