c606600dd3e8db4cb50e38b365ad1ca23650a1d9
Antoine <antoine.leca@laplateforme.io>
2025-01-15 12:00:28 +0100

    adding job05 file, testing

---
+++ jour03/job05/index.php
@@ -0,0 +1,23 @@
+<?php
+    $str = "On n'est pas le meilleur quand on le croit mais quand on le sait.";
+    $str_len = 0;
+    $keyC = 0;
+    $keyV = 0;
+    while (isset($str[$str_len])) {
+        $str_len++;
+    }
+    for ($val = 0 ; $val < $str_len ; $val++) {
+        $isvoyelle = false;
+        foreach ($voyelles as $voyelle) {
+            if ($str[$val] === $voyelle) {
+                $isvoyelle = true;
+                $keyV++;
+                echo $keyV;
+            }
+            else {
+                $keyC++;
+                echo $keyC;
+            }
+        }
+    }
+?>
