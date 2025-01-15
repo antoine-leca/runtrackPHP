60b2f133ac7d6016ce1990cdae69e2d6e4d68a0e
Antoine <antoine.leca@laplateforme.io>
2025-01-15 10:34:48 +0100

    job03, testing

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -1,7 +1,7 @@
 <?php
     $str = [
         "I", "'", "m ",  " s", "o", "r", "r", "y ",  " D", "a", "v", "e ",  " I", "'", "m ",  " a", "f", "r", "a", "i", "d ",  " I ",  " c", "a", "n", "'", "t ",  " d", "o ",  " t", "h", "a", "t"];
-        if ($val == "a" || $val == "e" || $val == "i" || $val == "o" || $val == "u" || $val == "y") {
-            echo $str[$val];
+        if ($str == "a" || $str == "e" || $str == "i" || $str == "o" || $str == "u" || $str == "y") {
+            echo $str;
         }
 ?>
