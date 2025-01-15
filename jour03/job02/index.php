6b5b9038c819c9ef9f0f1ecc83657ebbd7db3294
Antoine <antoine.leca@laplateforme.io>
2025-01-15 11:36:06 +0100

    testing job02 with a simple string

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -1,6 +1,5 @@
 <?php
-    $str = [
-        "T", "o", "u", "s ", " c", "e", "s " , " i", "n", "s", "t", "a", "n", "t", "s ", " s", "e", "r", "o", "n", "t ", " p", "e", "r", "d", "u", "s ", " d", "a", "n", "s ", " l", "e ", " t", "e", "m", "p", "s ", " c", "o", "m", "m", "e ", " l", "e", "s ", " l", "a", "r", "m", "e", "s ", " s", "o", "u", "s ", " l", "a ", " p", "l", "u", "i", "e", "." ];
+    $str = ["T", "o", "u", "s ", " c", "e", "s " , " i", "n", "s", "t", "a", "n", "t", "s ", " s", "e", "r", "o", "n", "t ", " p", "e", "r", "d", "u", "s ", " d", "a", "n", "s ", " l", "e ", " t", "e", "m", "p", "s ", " c", "o", "m", "m", "e ", " l", "e", "s ", " l", "a", "r", "m", "e", "s ", " s", "o", "u", "s ", " l", "a ", " p", "l", "u", "i", "e", "." ];
     $val = 0;
     $str_len = 0;
     foreach ($str as $i) {
@@ -10,4 +9,11 @@
         echo $str[$val];
         $val += 2;
     endwhile;
+?>
+<?php
+    $str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
+    
+    for ($i = 0; $i < strlen($str); $i += 2) {
+        echo $str[$i];
+    }
 ?>
