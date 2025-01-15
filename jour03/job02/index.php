8d1d8aca9e9cf431e76e9cd4e31b9a6ddc8929da
Antoine <antoine.leca@laplateforme.io>
2025-01-15 11:37:11 +0100

    job02 finalised with simple str instead array

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -1,16 +1,4 @@
 <?php
-    $str = ["T", "o", "u", "s ", " c", "e", "s " , " i", "n", "s", "t", "a", "n", "t", "s ", " s", "e", "r", "o", "n", "t ", " p", "e", "r", "d", "u", "s ", " d", "a", "n", "s ", " l", "e ", " t", "e", "m", "p", "s ", " c", "o", "m", "m", "e ", " l", "e", "s ", " l", "a", "r", "m", "e", "s ", " s", "o", "u", "s ", " l", "a ", " p", "l", "u", "i", "e", "." ];
-    $val = 0;
-    $str_len = 0;
-    foreach ($str as $i) {
-        $str_len++;
-    }
-    while ($val < $str_len):
-        echo $str[$val];
-        $val += 2;
-    endwhile;
-?>
-<?php
     $str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
     
     for ($i = 0; $i < strlen($str); $i += 2) {
