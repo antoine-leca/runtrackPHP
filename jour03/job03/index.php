0bb13b62313e7c38813d76a10f3e279dde060c3c
Antoine <antoine.leca@laplateforme.io>
2025-01-15 10:58:33 +0100

    job03, testing

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -1,6 +1,6 @@
 <?php
     $str = "I'm sorry Dave I'm afraid I can't do that";
-    $voyelles = ["a", "e", "i", "o", "u", "y"];
+    $voyelles = ["a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y"];
     for ($val = 0; $val < strlen($str); $val++) {
         if (in_array($str[$val], $voyelles)) {
             echo $str[$val];
