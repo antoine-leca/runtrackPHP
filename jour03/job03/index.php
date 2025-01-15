f885727a13d2c0f1d0951050c32518f7a7a587aa
Antoine <antoine.leca@laplateforme.io>
2025-01-15 10:40:44 +0100

    job03, testing

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -1,7 +1,26 @@
 <?php
-    $str = [
-        "I", "'", "m ",  " s", "o", "r", "r", "y ",  " D", "a", "v", "e ",  " I", "'", "m ",  " a", "f", "r", "a", "i", "d ",  " I ",  " c", "a", "n", "'", "t ",  " d", "o ",  " t", "h", "a", "t"];
-        if ($str == "a" || $str == "e" || $str == "i" || $str == "o" || $str == "u" || $str == "y") {
-            echo $str;
-        }
+    $str = ["I", "'", "m ", " s", "o", "r", "r", "y ",  " D", "a", "v", "e ", " I", "'", "m ",  " a", "f", "r", "a", "i", "d ",  " I ", " c", "a", "n", "'", "t ",  " d", "o ",  " t", "h", "a", "t"];
+    switch($str):
+        case $str === "a":
+            echo "a";
+            break;
+        case $str === "e":
+            echo "e";
+            break;
+        case $str === "i":
+            echo "i";
+            break;
+        case $str === "o":
+            echo "o";
+            break;
+        case $str === "u":
+            echo "u";
+            break;
+        case $str === "y":
+            echo "y";
+            break;
+        default:
+            echo " ";
+            break;
+        endswitch;
 ?>
