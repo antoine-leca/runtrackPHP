3954a179b6e9055ccc123498051cdc4597169c8b
Antoine <antoine.leca@laplateforme.io>
2025-01-17 11:29:08 +0100

    updating day05 job04 file with <br> and all tests, think finished

--- jour05/job04/index.php
+++ jour05/job04/index.php
@@ -2,21 +2,25 @@
     function calcule($num, $ope, $num2) {
     switch ($ope):
         case "+":
-            echo $num + $num2;
+            echo $num + $num2, "<br>";
             break;
         case "-":
-            echo $num - $num2;
+            echo $num - $num2, "<br>";
             break;
         case "*":
-            echo $num * $num2;
+            echo $num * $num2, "<br>";
             break;
         case "/":
-            echo $num / $num2;
+            echo $num / $num2, "<br>";
             break;
         default:
             echo "Choississez un opérateur valide";
             break;
     endswitch;
     }
-    calcule(5,"*",2)
+    calcule(5,"*",2);
+    calcule(5,"/",2);
+    calcule(5,"+",2);
+    calcule(5,"-",2);
+    calcule(5,"0",2);
 ?>
