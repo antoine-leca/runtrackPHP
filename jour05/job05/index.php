f2529d84d6bfd2c8775353598ba596db009773b2
Antoine <antoine.leca@laplateforme.io>
2025-01-17 12:42:02 +0100

    updating day05 job05 file, testing

--- jour05/job05/index.php
+++ jour05/job05/index.php
@@ -2,10 +2,13 @@
     function occurences($str, $char) {
         $str_len = 0;
         $char_counter = 0;
-        while (isset($str[$str_len($char)])) {
+        while (isset($str[$str_len])) {
             $str_len++;
-            $char_counter++;
-            return $char_counter;
+        }
+        for ($i = 0; $i < $str_len; $i++) {
+            if ($char < $str_len) {
+                $char_counter++;
+            }
         }
     }
     echo occurences("Bonjour", "o")
