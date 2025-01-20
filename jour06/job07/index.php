f49575a4bd5aa7d30600701ccbbad2339cdef45f
Antoine <antoine.leca@laplateforme.io>
2025-01-20 13:21:26 +0100

    adding day06 job07 file, testing

---
+++ jour06/job07/index.php
@@ -0,0 +1,11 @@
+<?php
+    function bubblesort($tab, $croissant) {
+        $tab = ["abc", "def", "ghi", "jkl", "mno"];
+        $tab_len = 0;
+        while (isset($tab[$tab_len])) {
+            $tab_len++;
+        }
+        
+
+    }
+?>
