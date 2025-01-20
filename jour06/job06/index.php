e3c193f4ff723d9aa1c85b9042607cd25f236ba4
Antoine <antoine.leca@laplateforme.io>
2025-01-20 12:35:55 +0100

    updating day06 job06 file, testing

--- jour06/job06/index.php
+++ jour06/job06/index.php
@@ -3,20 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulaire avec Styles</title>
+    <title>Document</title>
     <?php
-        $style = isset($_GET["style"]) ? $_GET["style"] : "style1";
+        $style = isset($_GET['style']) ? $_GET['style'] : 'style1';
         echo "<link id='stylesheet' rel='stylesheet' href='$style.css'>";
     ?>
 </head>
 <body>
+    <label for="">Style Switch</label>
     <form action="" method="get">
-        <label for="style">Choisissez un style :</label>
-        <select id="style" name="style">
-            <option value="styledef" <?php echo $style == "style0" ? "selected" : ""; ?>>Choisissez un style</option>
-            <option value="style 1" <?php echo $style == "style1" ? "selected" : ""; ?>>Style 1</option>
-            <option value="style 2" <?php echo $style == "style2" ? "selected" : ""; ?>>Style 2</option>
-            <option value="style 3" <?php echo $style == "style3" ? "selected" : ""; ?>>Style 3</option>
+        <select id="style" name="style">            
+            <option value="style0" <?php echo $style == 'style0' ? 'selected' : ''; ?>>Choisissez un style</option>
+            <option value="style1" <?php echo $style == 'style1' ? 'selected' : ''; ?>>Style 1</option>
+            <option value="style2" <?php echo $style == 'style2' ? 'selected' : ''; ?>>Style 2</option>
+            <option value="style3" <?php echo $style == 'style3' ? 'selected' : ''; ?>>Style 3</option>
         </select>
         <button type="submit">Appliquer</button>
     </form>
