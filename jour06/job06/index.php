a083d9ac944194a89048bf85c80190eadb8a3061
Antoine <antoine.leca@laplateforme.io>
2025-01-20 12:22:25 +0100

    updating day06 job06 file, testing

--- jour06/job06/index.php
+++ jour06/job06/index.php
@@ -6,7 +6,7 @@
     <title>Formulaire avec Styles</title>
     <?php
         $style = isset($_GET["style"]) ? $_GET["style"] : "style1";
-        echo '<link id="stylesheet" rel="stylesheet" href="$style.css">';
+        echo "<link id='stylesheet' rel='stylesheet' href='$style.css'>";
     ?>
 </head>
 <body>
@@ -14,9 +14,9 @@
         <label for="style">Choisissez un style :</label>
         <select id="style" name="style">
             <option value="styledef" <?php echo $style == "style0" ? "selected" : ""; ?>>Choisissez un style</option>
-            <option value="style1" <?php echo $style == "style1" ? "selected" : ""; ?>>Style 1</option>
-            <option value="style2" <?php echo $style == "style2" ? "selected" : ""; ?>>Style 2</option>
-            <option value="style3" <?php echo $style == "style3" ? "selected" : ""; ?>>Style 3</option>
+            <option value="style 1" <?php echo $style == "style1" ? "selected" : ""; ?>>Style 1</option>
+            <option value="style 2" <?php echo $style == "style2" ? "selected" : ""; ?>>Style 2</option>
+            <option value="style 3" <?php echo $style == "style3" ? "selected" : ""; ?>>Style 3</option>
         </select>
         <button type="submit">Appliquer</button>
     </form>
